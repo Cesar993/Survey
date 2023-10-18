5c4d758e16a894603190b9f308717f762f1ae62f
Cesar993 <ce.serrano@duocuc.cl.com>
2023-10-18 16:34:13 -0300

    Actualizacion usando formularios C#

--- Controllers/PrincipalController.cs
+++ Controllers/PrincipalController.cs
@@ -1,8 +1,8 @@
-
-// indicamos que utilizaremos aspnetcore y mvc
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
 
-namespace ProyectoPelis.controller;
+namespace DojoSurvey.Controllers;
 
 public class PrincipalController : Controller
 {
@@ -16,37 +16,32 @@ public static string com = "";
 public IActionResult Index(){
     return View("Index");
 }
+//Donde mosttramos la info
 [HttpGet]
 [Route("Alumnos")]
 public IActionResult Alumnos(){
-   
-    ViewBag.nombreNuevo = nombre;
-    ViewBag.dojoNuevo = dojo;
-    ViewBag.lengNuevo = leng;
-    ViewBag.comNuevo = com;
-    
+      
     return View("Alumnos");
 }
 
-
-
-[HttpPost]
-[Route("nuevo/alumno")]
-public RedirectToActionResult AgregarAlumno(string Nombre, string Dojo,string Lenguage, string Coments){
-    nombre =Nombre;
-    dojo =Dojo;
-    leng =Lenguage;
-    com =Coments;
-    return RedirectToAction("Alumnos");
-}
-
+// metodo para volve.<a href="/">Go back</a> es otra opcion
 [HttpGet]
 [Route("volver")]
-public RedirectToActionResult Volver(string Nombre, string Dojo,string Lenguage, string Coments){
-    nombre =Nombre;
-    
+public RedirectToActionResult Volver(){
+      
     return RedirectToAction("Index");
 }
+
+//metodo post donde en base a la clase Survey, 
+//creamos un objeto yoursurvey que toma los datos desde post del html
+[HttpPost("survey")]
+public IActionResult Submission(Survey yourSurvey)
+{
+
+ // hacia donde retornamos una vez los datos son enviados
+    return View("Alumnos",yourSurvey);
+}
+
 
 
 
---
+++ Models/Survey.cs
@@ -0,0 +1,10 @@
+#pragma warning disable CS8618
+namespace DojoSurvey.Models;
+
+public class Survey
+{
+        public string Name {get;set;}
+        public string Dojo{get;set;}
+        public string Lenguage{get;set;}
+        public string Comment{get;set;}
+}
